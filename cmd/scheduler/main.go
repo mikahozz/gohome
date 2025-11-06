2d767827d7aa00b0d0e0566b2c75130dcccef3a9
Mika Ahopelto <mika.ahopelto@gmail.com>
2025-11-06 20:49:04 +0200

    Added additional light schedules and deleted dbsync.

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/mikahozz/gohome/integrations/shelly"
 	"github.com/mikahozz/gohome/integrations/sun"
@@ -22,16 +23,38 @@ func main() {
 
 	scheduler := NewScheduler()
 	scheduler.AddSchedule(&DailySchedule{
-		Name:     "Night lights ON",
+		Name:     "Night lights ON at sunset",
 		Category: "night_lights",
 		Trigger: Trigger{
 			Time: sun.GetSunsetToday,
 		},
 		Action: func(ctx context.Context) error { return shelly.TurnOn(ctx) },
 	})
+	scheduler.AddSchedule(&DailySchedule{
+		Name:     "Night lights OFF at 23:00",
+		Category: "night_lights",
+		Trigger: Trigger{
+			Time: func() time.Time {
+				now := time.Now()
+				return time.Date(now.Year(), now.Month(), now.Day(), 23, 0, 0, 0, now.Location())
+			},
+		},
+		Action: func(ctx context.Context) error { return shelly.TurnOff(ctx) },
+	})
 
 	scheduler.AddSchedule(&DailySchedule{
-		Name:     "Night lights OFF",
+		Name:     "Morning lights ON at 6:45",
+		Category: "night_lights",
+		Trigger: Trigger{
+			Time: func() time.Time {
+				now := time.Now()
+				return time.Date(now.Year(), now.Month(), now.Day(), 6, 45, 0, 0, now.Location())
+			},
+		},
+		Action: func(ctx context.Context) error { return shelly.TurnOn(ctx) },
+	})
+	scheduler.AddSchedule(&DailySchedule{
+		Name:     "Morning lights OFF at sunrise",
 		Category: "night_lights",
 		Trigger: Trigger{
 			Time: sun.GetSunriseToday,
